1ffa9af97bef11a45f2ba5e475f80990c1d90d7c
Idada Osaigbovo James <wijojames@gmail.com>
2024-06-25 00:55:12 +0100

    feat: worked on the update todo endpoint and saved to mongo

--- main.go
+++ main.go
@@ -104,6 +104,21 @@ func createTodo(c *fiber.Ctx) error {
 	return c.Status(201).JSON(todo)
 }
 func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	ObjectID, err := primitive.ObjectIDFromHex(id)
 
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+	}
+
+	filter := bson.M{"_id":ObjectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
+
+	_, err = collection.UpdateOne(context.Background(),filter,update)
+
+	if err != nil{
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
 // func deleteTodo(c *fiber.Ctx) error {}
